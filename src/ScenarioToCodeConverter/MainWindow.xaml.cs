3eb89822cda4b63d7724cf77141ee49fb660e6ea
Andrei Shenets <andrei.shenets@itechart-group.com>
2017-10-12 11:59:42 +0300

    Migrated tool to generate test methods

---
+++ src/ScenarioToCodeConverter/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace ScenarioToCodeConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGenerateImplementationWitchCopyToClipboard(object sender, RoutedEventArgs e)
+        {
+            string sourceCode = SourceTextBox.Text;
+            string result = SimpleBdd.ScenarioToCodeConverter.Convert(sourceCode);
+            ResultTextBox.Text = result;
+            Clipboard.SetText(result, TextDataFormat.UnicodeText);
+        }
+
+        private void OnGenerateMethodNameFromString(object sender, RoutedEventArgs e)
+        {
+            string sourceCode = SourceTextBox.Text;
+            string result = SimpleBdd.ScenarioToCodeConverter.GetMethodNameFromString(sourceCode);
+            ResultTextBox.Text = result;
+            Clipboard.SetText(result, TextDataFormat.UnicodeText);
+        }
+    }
+}
